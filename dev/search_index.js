4588db204eccb0e8e16057f6b448a7aad1861dd4
Documenter.jl <documenter@juliadocs.github.io>
2023-10-25 06:13:29 +0000

    build based on 04cc6d5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlasmaTrace","category":"page"},{"location":"#PlasmaTrace","page":"Home","title":"PlasmaTrace","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlasmaTrace.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlasmaTrace]","category":"page"}]
+}
